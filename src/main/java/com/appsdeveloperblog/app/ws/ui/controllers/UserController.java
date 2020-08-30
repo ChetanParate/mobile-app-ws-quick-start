e34883dd4ea11ef96cf1cee95332e411475a5df8
cparate <chetan.parate@searshc.com>
2020-08-30 13:54:03 -0500

    updated valid pom dependuncy

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controllers/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controllers/UserController.java
@@ -2,13 +2,12 @@ package com.appsdeveloperblog.app.ws.ui.controllers;
 
 import java.util.Map;
 
-//import javax.validation.Valid;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
